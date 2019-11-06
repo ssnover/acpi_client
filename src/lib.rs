5e15ea2dc1ea819e1594580205aeb281e5a369d2
ssnover95 <sws7379@rit.edu>
2019-11-06 10:38:13 -0700

    Parse most of the power supply files to get their data

---
+++ src/lib.rs
@@ -0,0 +1,96 @@
+use std::error::Error;
+use std::fs;
+use std::io::prelude::*;
+
+pub struct PowerSupplyInfo {
+    pub name: String,
+    pub remaining_capacity: Option<u32>,
+    pub remaining_energy: Option<u32>,
+    pub present_rate: Option<u32>,
+    pub voltage: Option<u32>,
+    pub design_capacity: Option<u32>,
+    pub design_capacity_unit: Option<u32>,
+    pub last_capacity: Option<u32>,
+    pub last_capacity_unit: Option<u32>,
+    // pub hours: Option<u32>,
+    // pub minutes: Option<u32>,
+    // pub seconds: Option<u32>,
+    // pub percentage: Option<u32>,
+    pub is_battery: bool,
+    // capacity_unit: String,
+}
+
+pub fn get_power_supply_info() -> Result<Vec<PowerSupplyInfo>, Box<dyn Error>> {
+    let mut results: Vec<PowerSupplyInfo> = vec![];
+    let power_supply_path = "/sys/class/power_supply";
+
+    for entry in fs::read_dir(&power_supply_path)? {
+        results.push(PowerSupplyInfo::new(&entry?)?);
+    }
+
+    Ok(results)
+}
+
+impl PowerSupplyInfo {
+    pub fn new(entry: &fs::DirEntry) -> Result<PowerSupplyInfo, Box<dyn Error>> {
+        let name = entry.file_name().into_string().unwrap();
+
+        let remaining_capacity = parse_file_to_u32(entry, "charge_now", 1000)?;
+        let remaining_energy = parse_file_to_u32(entry, "energy_now", 1000)?;
+        let present_rate = parse_file_to_u32(entry, "current_now", 1000)?;
+        let voltage = parse_file_to_u32(entry, "voltage_now", 1000)?;
+        let design_capacity = parse_file_to_u32(entry, "charge_full_design", 1000)?;
+        let design_capacity_unit = parse_file_to_u32(entry, "energy_full_design", 1000)?;
+        let last_capacity = parse_file_to_u32(entry, "charge_full", 1000)?;
+        let last_capacity_unit = parse_file_to_u32(entry, "energy_full", 1000)?;
+        let is_battery = match parse_entry_file(entry, "type")? {
+            Some(val) => val == "battery",
+            None => false,
+        };
+
+        Ok(PowerSupplyInfo {
+            name,
+            remaining_capacity,
+            remaining_energy,
+            present_rate,
+            voltage,
+            design_capacity,
+            design_capacity_unit,
+            last_capacity,
+            last_capacity_unit,
+            is_battery,
+        })
+    }
+}
+
+fn parse_entry_file(
+    entry: &fs::DirEntry,
+    file: &'static str,
+) -> Result<Option<String>, Box<dyn Error>> {
+    let mut path = entry.path();
+    path.push(file);
+    let mut result = String::new();
+
+    if path.is_file() {
+        if let Some(filename) = path.to_str() {
+            let mut f = fs::File::open(filename)?;
+            f.read_to_string(&mut result)?;
+            let contents = result.trim();
+            return Ok(Some(String::from(contents)));
+        }
+    }
+
+    Ok(None)
+}
+
+fn parse_file_to_u32(
+    entry: &fs::DirEntry,
+    file: &'static str,
+    scalar: u32,
+) -> Result<Option<u32>, Box<dyn Error>> {
+    let result = match parse_entry_file(entry, file)? {
+        Some(val) => Some(val.parse::<u32>()? / scalar),
+        None => None,
+    };
+    Ok(result)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,14 @@
-use std::fs;
-use std::io::Read;
-
-struct PowerSupplyInfo {
-    remaining_capacity: Option<u32>,
-    remaining_energy: Option<u32>,
-    present_rate: Option<u32>,
-    voltage: Option<u32>,
-    design_capacity: Option<u32>,
-    design_capacity_unit: Option<u32>,
-    last_capacity: Option<u32>,
-    last_capacity_unit: Option<u32>,
-    hours: Option<u32>,
-    minutes: Option<u32>,
-    seconds: Option<u32>,
-    percentage: Option<u32>,
-    is_battery: bool,
-    capacity_unit: String,
-}
-
-impl Default for PowerSupplyInfo {
-    fn default() -> PowerSupplyInfo {
-        PowerSupplyInfo { 
-            remaining_capacity: None,
-            remaining_energy: None,
-            present_rate: None,
-            voltage: None,
-            design_capacity: None,
-            design_capacity_unit: None,
-            last_capacity: None,
-            last_capacity_unit: None,
-            hours: None,
-            minutes: None,
-            seconds: None,
-            percentage: None,
-            is_battery: false,
-            capacity_unit: "mAh".to_string() 
-        }
-    }
-}
-
 fn main() -> std::io::Result<()> {
-    let acpi_path = "/sys/class".to_string();
-    let power_supply_path = acpi_path + "/power_supply";
 
-    for entry in fs::read_dir(&power_supply_path)? {
-        let resource = (entry?).file_name().into_string().unwrap();
-        let mut supply = PowerSupplyInfo { ..Default::default() };
-        println!("{:}", resource);
-        let resource_path = format!("{}/{}", &power_supply_path, resource);
-        for entry in fs::read_dir(&resource_path)? {
-            let resource_filename = (entry?).file_name().into_string().unwrap();
-            println!("  {:}", resource_filename); 
-            let entry_path = format!("{}/{}", &resource_path, resource_filename);
-            match get_file_from_path(&entry_path) {
-               Ok(mut f) => {
-                   let mut contents = String::new();
-                   f.read_to_string(&mut contents)?;
-                   match resource_filename.as_ref() {
-                       "type" => {
-                           println!("Found the type!"); 
-                           if contents == "battery" {
-                               supply.is_battery = true;
-                           }
-                       },
-                       _ => {},
-                   }
-               },
-               Err(_e) => { continue; },
-            }
+    let _power_supplies: Vec<acpi_client::PowerSupplyInfo> = match acpi_client::get_power_supply_info() {
+        Ok(ps) => ps,
+        Err(e) => {
+            eprintln!("Application error: {}", e);
+            std::process::exit(1);
         }
-        if supply.is_battery {
-            println!("{} {}", "Battery", 1);
-        }
-    }
+    };
+
     Ok(())
+
 }
 
-fn get_file_from_path(path: &String) -> std::result::Result<fs::File, &'static str> {
-    let metadata = fs::metadata(&path).unwrap();
-    let file_type = metadata.file_type();
-
-    if file_type.is_dir() {
-        return Err("File is a directory");
-    } else if file_type.is_symlink() {
-        return Err("File is a symlink");
-    } else {
-        return match fs::File::open(path) {
-            Ok(f) => { Ok(f) },
-            Err(e) => {
-                eprintln!("Error: {}", e);
-                Err("Could not open file")
-            },
-        };
-    }
-}
