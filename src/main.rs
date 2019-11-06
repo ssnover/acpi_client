e2cc7c79f018f57d75380ecbdba2879ee2cd19a9
ssnover95 <sws7379@rit.edu>
2019-11-06 10:39:26 -0700

    Format

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 fn main() -> std::io::Result<()> {
-
-    let _power_supplies: Vec<acpi_client::PowerSupplyInfo> = match acpi_client::get_power_supply_info() {
-        Ok(ps) => ps,
-        Err(e) => {
-            eprintln!("Application error: {}", e);
-            std::process::exit(1);
-        }
-    };
+    let _power_supplies: Vec<acpi_client::PowerSupplyInfo> =
+        match acpi_client::get_power_supply_info() {
+            Ok(ps) => ps,
+            Err(e) => {
+                eprintln!("Application error: {}", e);
+                std::process::exit(1);
+            }
+        };
 
     Ok(())
-
 }
-
